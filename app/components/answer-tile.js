d4cb46341b2014e3616d5199b322ee0fab051e7b
Betty Mutai <bettymutai00@gmail.com>
2017-06-16 14:26:27 +0300

    Added a service for users to be able to add answers they want to view later

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('answer.author', 'answer.content', function() {
+   return this.get('answer.author') + ' - ' + this.get('answer.content');
+ }),
+
+  myReference: Ember.inject.service(),
+  
   actions: {
+    addToReference(answer) {
+      this.get('myReference').add(answer);
+    },
    delete(answer) {
      if (confirm('Are you sure you want to delete this answer?')) {
        this.sendAction('destroyAnswer', answer);
--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -6,6 +6,9 @@ export function questionPopularity(params/*, hash*/) {
   if(question.get('answers').get('length') >= 3) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-up"></span>');
   }
+  else {
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-down"></span>');
+    }
 }
 
 export default Ember.Helper.helper(questionPopularity);
